838132f84061bfa45ba8fa96b0ff23d06f9449fb
Rischae <104549627+mnrichae@users.noreply.github.com>
2022-06-24 19:37:37 +0800

    added some minimal design

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,15 +2,14 @@ import { useState, useEffect } from 'react';
 import * as React from 'react';
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
-// import loginImg from '../assets/login.png'
+import loginImg from '../assets/login.png'
 import Button from '@mui/material/Button';
 
 //css
-// import './css/login.css'
+import '../css/forms.css'
 
 
 function Login() {
-
     const initialValues = { email: "", password: ""};
     const [formValues, setFormValues] = useState(initialValues);
     const [formErrors, setFormErrors] = useState({});
@@ -36,77 +35,70 @@ function Login() {
     const validate = (values) => {
         const errors = {};
         const regex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i;
-
-        //local storage validation
         const emailInput = formValues.email;
         const passwordInput = formValues.password;
-        console.log(passwordInput);
-        const getEntries = JSON.parse(localStorage.getItem("Entries"));
+        const getEntries = JSON.parse(localStorage.getItem("Users"));
         const filteredEntries = getEntries.filter((item) => {
             return emailInput === item.email && passwordInput === item.password;
         });
-        console.log(filteredEntries);
 
-        //conditional statements 
-            // for syntax 
+
         if (!values.email){
-            errors.email = "Please provide an email."
+            errors.email = `Please provide an email.`
         } else if (!regex.test(values.email)){
             errors.email = "Please provide a valid email format.";
         } 
-
         if (!values.password){
             errors.password = "Please provide a password.";
         } else if (values.password.length < 4) {
             errors.password = "Password must be more than 4 characters";
         }
-
         if (filteredEntries.length === 0){
-            errors.filteredEntries = "Invalid Email/password";
+            errors.filteredEntries = "Record not found";
             console.log(`error`)
         } 
-        console.log(`checking validate function ${errors}`);
         return errors;
     }
 
 
     return (
-        <div className="container">
-             {Object.keys(formErrors).length === 0 && isSubmit ? (
-                            console.log('proceed to home')
-                        ) : (
-                            console.log('login error')
-                        )}
-
+        <div className="formLogin">
             <Box sx={{ flexGrow: 1 }} className="boxContainer">
             <Grid container spacing={0}>
                 <Grid item xs={6} className="loginImgGrid">
-                    {/* <img src={loginImg} alt="" className="loginImg"/> */}
+                    <img src={loginImg} alt="" className="loginImg"/>
                 </Grid>
                 <Grid item xs={6} className="loginFormGrid">
-                    <h1 className="loginName">Login</h1>
+                    <h1 className="formLoginName">Login</h1>
                     <form className="loginForm" onSubmit={handleSubmit} noValidate>
-                        <label htmlFor="email" className="emailLabel"><h2 className="emailH3">Email</h2></label>
+                        <label htmlFor="email" className="emailLabel"><h2 className="loginLabels">Email</h2></label>
                         <input 
                             type="email" 
                             name="email" 
                             id="email" 
+                            className="formInput"
                             value={formValues.email}
                             onChange={handleChange}
-                            autocomplete="off"/><br />
-                        <p className="errorEmail">{formErrors.email}</p>
+                            autoComplete="off"/><br />
+                        <p className="errorMsg">{formErrors.email}</p>
 
-                        <label htmlFor="password"><h2 className="passwordH3">Password</h2></label>
+                        <label htmlFor="password"><h2 className="loginLabels loginLabelPassword">Password</h2></label>
                         <input 
                             type="password" 
                             name="password" 
                             id="password" 
+                            className="formInput"
                             value={formValues.password}
                             onChange={handleChange}
-                            autocomplete="off" />
-                        <p className="errorPassword">{formErrors.password}</p>
+                            autoComplete="off" />
+                        <p className="errorMsg">{formErrors.password}</p>
                         <br />
                         <p className="errorMsg">{formErrors.filteredEntries}</p>
+                        {Object.keys(formErrors).length === 0 && isSubmit ? (
+                            <p className="successMsg">Signed in successfully</p>
+                        ) : (
+                            console.log('login error')
+                        )}
                         <Button variant="contained" type="submit" className="loginBtn">LOGIN</Button>
 
                        
