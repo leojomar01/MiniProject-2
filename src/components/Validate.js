435cdc2c40124328708ee993a33382a47591cb11
Rischae <104549627+mnrichae@users.noreply.github.com>
2022-06-24 19:39:17 +0800

    Debug for local storage
    Updated code to ensure smooth local storage saving and retrieval

--- src/components/Validate.js
+++ src/components/Validate.js
@@ -1,188 +1,177 @@
-import { useState, useEffect } from 'react';
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import Grid from '@mui/material/Grid';
-// import loginImg from '../assets/login.png'
+import React, { useEffect, useState } from "react";
 import Button from '@mui/material/Button';
-
 //css
-// import './css/login.css'
+import '../css/forms.css'
 
-const retrieveRegEntries = localStorage.getItem('Entries') ? JSON.parse(localStorage.getItem('Entries')) : ['']; 
 function Validate() {
-
-    const regInitialValues = { fName: "", lName: "", email: "", password: "", cPassword: "", job: ""}; //setInitialValues
-    const [regValues, setRegValues] = useState(regInitialValues); //setFormValues
-    // eslint-disable-next-line
-    const [regFormErrors, setRegFormErrors] = useState({}); //SetFormErrors
-    const [isRegSubmit, setIsRegSubmit] = useState(false); //isSubmit
-    const [allRegEntries, setAllRegEntries] = useState(retrieveRegEntries);
-
-
-    const handleChange = (e) => {
-        const { name, value } = e.target;
-        setRegValues({ ...regValues, [name]: value});
-        console.log(`setRegValues from handleChange`);
-    };
-
+    const [errors, setErrors] = useState({});
+    const [formUser, setFormUser] = useState({ firstName: "", lastName: "", email: "", password: "", confirmPassword: "", job: "" });
+    const [users, setUsers] = useState([]);
+    const [isSuccess, setIsSuccess] = useState(false);
+  
     const handleSubmit = (e) => {
-        e.preventDefault();
-        setRegFormErrors(displayError(regValues));
-        setIsRegSubmit(true);
-        setAllRegEntries([...allRegEntries,regValues]);
-        // setRegValues('');
+      console.log("saving...", formUser);
+      users.push(formUser);
+      localStorage.setItem("Users", JSON.stringify(users));
+      setIsSuccess(true);
+      
     };
 
-    useEffect(() => {
-        if (Object.keys(regFormErrors).length === 0 && isRegSubmit) {
-            localStorage.setItem('Entries', JSON.stringify(allRegEntries));
-            console.log(allRegEntries);
-        } // eslint-disable-next-line
-    }, [regFormErrors]);
-
-    const displayError = (values) => {
-        const regErrors = {};
+    const checkValidEmail = (email) => {
         const regRegex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i;
-
-        const regEmailInput = regValues.email;
-        console.log(regEmailInput);
-        const getRegEntries = JSON.parse(localStorage.getItem("Entries"));
-        const filteredRegEntries = getRegEntries.filter((item) => {
-            return regEmailInput === item.email;
-        });
-        console.log(filteredRegEntries);
-       
-
-        //conditional statements 
-            // for syntax 
-        if (!values.fName){
-            regErrors.fName = "Please provide your first name."
+        if(!regRegex.test(email)){
+            return `Invalid email format`;
+        } else if (
+            users.length > 0 &&
+            users.filter((user) => user.email === email).length > 0
+          ) {
+            return `Email already exists. Sign in instead`;
+          } else {
+          return "";
         }
-
-        if (!values.lName){
-            regErrors.lName = "Please provide your last name."
-        }
-
-        if (!values.email){
-            regErrors.email = "Please provide an email."
-        } else if (!regRegex.test(values.email)){
-            regErrors.email = "Please provide a valid email format.";
-        } 
-
-        if (!values.password){
-            regErrors.password = "Please provide a password.";
-        } else if (values.password.length < 4) {
-            regErrors.password = "Password must be more than 4 characters";
-        }
-
-        if (!values.cPassword){
-            regErrors.cPassword = "Please confirm your password.";
-        } else if (values.cPassword !== values.password){
-            regErrors.cPassword = "Passwords did not match."
-        }
-
-        if (!values.job){
-            regErrors.job = "Please provide your job."
-        }
-
-        if (filteredRegEntries.length > 0){
-            regErrors.filteredRegEntries = "Email is already registered. Login instead.";
-            console.log(`filteredRegEntries`)
-        } 
-        console.log(`checking validate function ${regErrors}`);
-        return regErrors;
     }
 
+    const checkPasswordFormat = (password) => {
+        if(password.length < 4) {
+            return `Password must be more than 4 characters`
+        } 
+        return "";
+    }
 
+    const checkMatchPassword = (password, confirmPassword) => {
+        if (
+            confirmPassword !== password
+        ) {
+            return `Passwords did not match`;
+        }
+        return "";
+    }
+  
+    const validate = (e) => {
+      setErrors({
+        firstNameError:
+          formUser.firstName === ""
+            ? "First Name is required."
+            : "",
+
+        lastNameError: 
+            formUser.lastName === "" 
+            ? "Last Name is required." 
+            : "",
+
+        emailError:
+            formUser.email === ""
+            ? "Email is required."
+            : checkValidEmail(formUser.email),  
+            
+        passwordError:
+            formUser.password === ""
+            ? "Password is required."
+            : checkPasswordFormat(formUser.password),
+
+        confirmPasswordError:
+            formUser.confirmPassword === ""
+            ? "Password confirmation is required."
+            : checkMatchPassword(formUser.password,formUser.confirmPassword),
+            
+        jobError:
+            formUser.job === ""
+            ? "Job is required."
+            : "",
+      });
+    };
+    
+
+    useEffect(() => {
+      const storedUsers = localStorage.getItem("Users")
+        ? JSON.parse(localStorage.getItem("Users"))
+        : [];
+      setUsers(storedUsers);
+    }, []);
+  
+    useEffect(() => {
+      if (errors.firstNameError === "" && errors.lastNameError === "" && errors.emailError === "" && errors.passwordError === "" && errors.confirmPasswordError === "" && errors.jobError === "")
+        handleSubmit();
+        // eslint-disable-next-line
+    }, [errors]);
+  
+    const handleChange = (e) => {
+      setFormUser({ ...formUser, [e.target.name]: e.target.value });
+    };
+  
     return (
-        <div className="container">
-             {Object.keys(regFormErrors).length === 0 && isRegSubmit ? (
-                            console.log('proceed to login')
-                        ) : (
-                            console.log('login error')
-                        )}
+      <div className="formValidate">
+        <h1 className="formValidateName">Registration</h1>
+        {/* First Name */}
+        <label htmlFor= "firstName">First Name</label>
+        <input
+          name="firstName"
+          type="text"
+          className="formInput"
+          onChange={handleChange}
+          autoComplete="off"
+        />
+        <p className="errorMsg">{errors.firstNameError}</p>
 
-            <Box sx={{ flexGrow: 1 }} className="boxContainer">
-            <Grid container spacing={0}>
-                <Grid item xs={6} className="loginImgGrid">
-                    {/* <img src={loginImg} alt="" className="loginImg"/> */}
-                </Grid>
-                <Grid item xs={6} className="loginFormGrid">
-                    <h1 className="loginName">Register Example</h1>
-                    <form className="loginForm" onSubmit={handleSubmit} noValidate>
+        {/* Last Name */}
+        <label htmlFor= "lastName">Last Name</label>
+        <input
+          name="lastName"
+          type="text"
+          className="formInput"
+          onChange={handleChange}
+          autoComplete="off"
+        />
+        <p className="errorMsg">{errors.lastNameError}</p>
 
-                        <label htmlFor="fName" className="fName">First Name</label>
-                        <input 
-                            type="text" 
-                            name="fName"  
-                            value={regValues.fName}
-                            onChange={handleChange}
-                            autoComplete="off"/><br />
-                        <p className="errorMsg">{regFormErrors.fName}</p>
+        {/* Email */}
+        <label htmlFor= "email">Email</label>
+         <input
+          name="email"
+          type="email"
+          className="formInput"
+          onChange={handleChange}
+          autoComplete="off"
+        />
+        <p className="errorMsg">{errors.emailError}</p>
 
-                        <label htmlFor="lName" className="lName">Last Name</label>
-                        <input 
-                            type="text" 
-                            name="lName" 
-                            value={regValues.lName}
-                            onChange={handleChange}
-                            autoComplete="off"/><br />
-                        <p className="errorMsg">{regFormErrors.lName}</p>
+        {/* Password */}
+        <label htmlFor= "password">Password</label>
+        <input
+          name="password"
+          type="password"
+          className="formInput"
+          onChange={handleChange}
+          autoComplete="off"
+        />
+        <p className="errorMsg">{errors.passwordError}</p>
 
+         {/* Confirm Password */}
+         <label htmlFor= "confirmPassword">Confirm Password</label>
+         <input
+          name="confirmPassword"
+          type="password"
+          className="formInput"
+          onChange={handleChange}
+          autoComplete="off"
+        />
+        <p className="errorMsg">{errors.confirmPasswordError}</p>
 
-                        <label htmlFor="email" className="emailLabel">Email</label>
-                        <input 
-                            type="email" 
-                            name="email" 
-                            value={regValues.email}
-                            onChange={handleChange}
-                            autoComplete="off"/><br />
-                        <p className="errorMsg">{regFormErrors.email}</p>
+        {/* Job */}
+        <label htmlFor= "job">Job</label>
+        <input
+          name="job"
+          type="text"
+          className="formInput"
+          onChange={handleChange}
+          autoComplete="off"
+        />
+         <p className="errorMsg">{errors.jobError}</p>
 
-                        <label htmlFor="password">Password</label>
-                        <input 
-                            type="password" 
-                            name="password" 
-                            value={regValues.password}
-                            onChange={handleChange}
-                            autoComplete="off" />
-                        <p className="errorMsg">{regFormErrors.password}</p>
-
-
-                        <label htmlFor="cPassword">Confirm Password</label>
-                        <input 
-                            type="password" 
-                            name="cPassword" 
-                            value={regValues.cPassword}
-                            onChange={handleChange}
-                            autoComplete="off" />
-                        <p className="errorMsg">{regFormErrors.cPassword}</p>
-
-                        <label htmlFor="job">Job</label>
-                        <input 
-                            type="text" 
-                            name="job" 
-                            value={regValues.job}
-                            onChange={handleChange}
-                            autoComplete="off" />
-                        <p className="errorMsg">{regFormErrors.job}</p>
-                        <br />
-                        
-                        
-
-
-                        <p className="errorMsg">{regFormErrors.filteredRegEntries}</p>
-                        <Button variant="contained" type="submit" className="loginBtn">Register</Button>
-
-                       
-                        <br /> <br />
-                        {/* <Link to = "/registration">Not yet a member?</Link> */}
-                    </form>
-                </Grid>
-            </Grid>
-            </Box>
-        </div>
-      );
+        {isSuccess && <p className="successMsg">Registered Successfully!</p>}
+        <Button variant="contained" onClick={validate} className="validateBtn">Register</Button>
+      </div>
+    );
 }
 
-export default Validate
+export default Validate;
